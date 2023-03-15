bf33385f5fa3ef461860b3a337495110579ecaa6
jana-simjanovska <jana.simjanovska@sourcico.com>
2023-03-15 16:26:05 +0100

    initial painfully ugly design

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,33 @@
 import React from "react";
-import "./App.css";
+import "./App.scss";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="code-editor-container">
+        <header>NAJJAK VISUALIZER EVER</header>
+        <div className="actions-container">
+          <select name="code-sample" id="code-sample">
+            <option>Choose an example</option>
+          </select>
+          <button>Run/Edit</button>
+          <button>Step</button>
+          <button>Play</button>
+        </div>
+      </div>
+      <div className="execution-visualizer-container">
+        <div className="ne-znam">
+          <div className="call-stack-container"></div>
+          <div className="right-test">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
